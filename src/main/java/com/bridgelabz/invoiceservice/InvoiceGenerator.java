32cb475c91d0f1c7e779f04647a0e9e861cea13d
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-24 15:45:51 +0530

    Gaurav[ADDED]: Calculate total fare method in main class for given time and Distance

--- src/main/java/com/bridgelabz/invoiceservice/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/invoiceservice/InvoiceGenerator.java
@@ -1,9 +1,26 @@
 package com.bridgelabz.invoiceservice;
 
 public class InvoiceGenerator {
+    private static final int MINIMUM_COST_PER_KM = 10;
+    private static final int MINIMUM_COST_PER_MINUTE = 1;
+    private static final int MINIMUM_FARE = 5;
+
     /*Function to print Welcome message*/
     public boolean printWelcomeMessage() {
         System.out.println("Welcome to Cab Invoice Generation System");
         return true;
+    }
+
+    /**
+     * @param distance
+     * @param time
+     * @return total Fare for given Distance and Time
+     */
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * MINIMUM_COST_PER_MINUTE;
+        if (totalFare < MINIMUM_FARE) {
+            return MINIMUM_FARE;
+        }
+        return totalFare;
     }
 }
